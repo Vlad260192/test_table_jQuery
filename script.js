0122f9a4ba270b089684915f9c95b8c9685c698c
Vlad260192 <v.kapustianskiy@gmail.com>
2022-06-22 15:59:46 +0300

    commit

---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict"
+// setTimeout (go, 5000);
+// function go() {
+//     alert ('Hello world!!!')
+// };
+
+// let age = 29;
+// let name = 'Vlad';
+
+// if (age > 16) {
+//     alert ('You are too old for this')
+// } else {
+//     ('Welcome to the club')
+// }
+
+
+// let vine = 3; // number type
+// let name = 'Pit'; // string type
+// let dark = false; // boolean type
+// let someVariable; // undefined
+// let myFirstName = 'Vlad';
+
+// let FirstName = 'Vlad ';
+// let SecondName = 'Kapustianskyi';
+// alert (FirstName + SecondName);
+
+
+// var price = 100;
+// var discont = 25;
+// var total = price - (price * (discont / 100)); // все що після равно нназивається виразом (виражение)
+// console.log(total);
+
+// var name = 'Vlad';
+// var secondName = 'Kapustianskyi';
+// var myInfo = name + secondName; // операція конкатенації (працює лише зі строковими елементами)
+// alert(myInfo);
+
+// var firstNumber = 22;
+// var secondNumber = '11';
+// var myInfo = firstNumber + secondNumber; // операція конкатенації (працює лише зі строковими елементами)
+// alert(myInfo);
+
+// var age = 20;
+// alert(age >= 20);
+
+// while, for, for in, forEach
+
+// var number = 50;
+// while(number > 0){
+//     document.write('Number more than 0 <br>');
+//     number = number - 1;
+// }
+// document.write('Number less than 0');
+
+// var apples = 5;
+
+// while(apples < 10){
+//     document.write('Give apple to my friend <br>')
+//     apples = apples + 1;
+// }
+// document.write('<br>Sorry, I don`t have apples');
+
+// let firstName = 'Dmytro';
+// alert (`Hi, ${firstName}!`);
+
+// let result = 2 + 1;
+// alert(`Total score: ${result}`);
+
+// let age = null;
+// alert(age);
+
+// number
+// string
+// boolean
+// undefined
+// null
+// bigInt
+// object
+// sumbol
+
+let name = 'Vlad';
+
+alert(`hello, ${"name"}`);
